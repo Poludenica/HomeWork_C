62ebb9bbd476e2b8cd392bd9bf30d39501d540ee
Poludnica <svasti.kaula@gmail.com>
2023-03-26 18:07:41 +0300

    First homework done

---
+++ Sem1/Program.cs
@@ -0,0 +1,65 @@
+﻿// ДЗ 23.03
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+// Console.Write("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if ( num1 > num2) 
+// { 
+//     Console.WriteLine($"Максимальное число {num1}, минимальное число {num2}");
+
+// }
+// else 
+// {
+//     Console.WriteLine($"Максимальное число {num2}, минимальное число {num1}");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// if ((num1 > num2)&&(num1 > num3))
+// {
+//     Console.WriteLine("Максимальное число " + num1);
+// }
+
+// else if ((num2 > num1)&&(num2 > num3))
+// {
+//     Console.WriteLine("Максимальное число " + num2);
+// }
+// else if ((num3 > num1)&&(num3 > num2))
+// {
+//     Console.WriteLine("Максимальное число " + num3);
+// }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Введите число ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n % 2 == 0)
+// {
+//     Console.WriteLine("Число является четным");
+// }
+// else
+// {
+//     Console.WriteLine("Число является нечетным");
+// }
+
+
+//Задача 4.  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 2; i <= n; i = i + 2)
+// {
+//     Console.Write(i + " ");
+// }
+
